8806f823e1e670323ba3e56147ad48845dd59647
p-parks <paul.anthony.parks@gmail.com>
2024-11-22 14:11:40 -0700

    Generate baseline metrics

--- SharedUtils.py
+++ SharedUtils.py
@@ -1,3 +1,7 @@
+import evaluate
+import json
+import os
+
 def trim_to_last_punctuation(text):
     # attempt to trim any of the cut off sentences
     # reverse find the last punctuation
@@ -19,3 +23,22 @@ def trim_to_max_length(text):
         text = text[:max__text_length]
     
     return trim_to_last_punctuation(text)
+
+results_dir = 'results'
+
+def evaluate_and_save_metrics(dir_name, run_name, model_name, reference_summaries, generated_summaries, elapsed_time):
+    os.makedirs(f"./results/{dir_name}/{run_name}", exist_ok=True)
+
+    rouge = evaluate.load('rouge')
+    rouge_results = rouge.compute(predictions=generated_summaries, references=reference_summaries)
+    bleu = evaluate.load('bleu')
+    results_bleu = bleu.compute(predictions=generated_summaries, references=reference_summaries)
+    
+    with open(f"./results/{dir_name}/{run_name}/gpt_{model_name}_rouge_results.json", "w") as f:
+        json.dump(rouge_results, f)
+    with open(f"./results/{dir_name}/{run_name}/gpt_{model_name}_bleu_results.json", "w") as f:
+        json.dump(results_bleu, f)
+    with open(f"./results/{dir_name}/{run_name}/gpt_{model_name}_time.txt", "w") as f:
+        f.write(f"{elapsed_time}\n")
+
+    return rouge_results, results_bleu 
